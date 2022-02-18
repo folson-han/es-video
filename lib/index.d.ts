d8fa7e2ad66b2845b32f46798bde7b426e18e68b
chen <ch.10234en@gmail.com>
2022-02-18 17:09:29 +0800

    添加基础结构

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,14 @@
-export declare const Greeter: (name: string) => string;
+declare function setStyle(elem: HTMLElement, obj: CSSStyleSheet): void;
+interface PlayerAdapterProps {
+}
+declare class PlayerAdapter {
+    private video;
+    constructor(props: PlayerAdapterProps);
+}
+interface PlayerProps {
+}
+declare class Player {
+    private player;
+    constructor(props: PlayerProps);
+    private videoEventInit;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,43 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+import { Properties } from "csstype";
+
+function setStyle(elem: HTMLElement, obj: Properties){
+    Object.assign(elem.style, obj);
+}
+
+interface PlayerAdapterProps{
+    src: string;
+}
+
+class PlayerAdapter{
+    private readonly video: HTMLVideoElement;
+    constructor(props: PlayerAdapterProps) {
+        this.video = window.document.createElement("video");
+
+        setStyle(this.video, {
+            width: "100%",
+            height: "100%",
+        });
+
+        this.video.setAttribute("src", props.src);
+    }
+
+    public onError(){
+
+    }
+}
+
+interface PlayerProps{
+
+}
+
+class Player{
+    private player: HTMLDivElement;
+    constructor(props: PlayerProps) {
+        this.player = window.document.createElement("div");
+
+    }
+
+    private videoEventInit(){
+
+    }
+}
